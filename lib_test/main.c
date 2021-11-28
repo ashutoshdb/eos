7746b6cac50c001d7569602f1ae30f35faad3dbe
ashutoshdb <ashutoshdubey607@gmail.com>
2021-11-28 10:18:46 +0530

    dynamic lib added

---
+++ lib_test/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"airth.h"
+
+int main(){
+int a = 10;
+int b =10;
+int sum;
+
+sum = add(a,b);
+printf("sum = %d",sum);
+
+
+}
---
+++ lib_test_dynamic/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int a = 10;
+int b = 10;
+int sum;
+
+sum = add(a,b);
+printf("sum = %d",sum);
+
+
+}
