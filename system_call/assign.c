d3a73c70e470f249f215f3ec53af387e8baca103
ashutoshdb <ashutoshdubey607@gmail.com>
2021-11-30 22:06:52 +0530

    day 5 assignment

---
+++ system_call/assign.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+  #include <sys/types.h>
+       #include <sys/stat.h>
+       #include <fcntl.h>
+       #include <stdlib.h>
+#include<unistd.h>
+#include<errno.h>
+
+
+
+// ./rojbin source.txt dest.txt
+int main(int argc, char const *argv[])
+{
+    int fdr, fdw;
+    char buffer[50];
+    fdr = open(argv[1], O_RDONLY);
+    fdw = open(argv[2], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    if(-1 == fdr | -1 == fdw){
+           perror("Error \n:");
+           exit(EXIT_FAILURE);
+
+    }
+    read(fdr,buffer,50);
+    write(fdw, buffer,sizeof(buffer));
+    return 0;
+}
--- system_call/system_call.c
+++ system_call/system_call.c
@@ -1,3 +1,25 @@
+#include<stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h> 
+#include <stdlib.h>
+#include<unistd.h>
+#include<errno.h>
+#include<fcntl.h>
+
+
+int main()
+{
+    int fd;
+    fd = open("desd.txt", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR); 
+    if(-1 == fd){
+        perror("Error \n:");
+        exit(EXIT_FAILURE);
+    }
+    printf("sddad");
+    write(fd, "DESD", 4);
+    close(fd);
+
+    return 0;
+
+}
